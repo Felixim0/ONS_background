cdfd5a449991db7ff35c4b08c9cbac75cb1deb4d
Felixim0 <aldam.gatesfelix@gmail.com>
2025-05-24 00:32:17 +0100

    Added bubbles on the left at the right size shape and colour

--- background.js
+++ background.js
@@ -4,7 +4,26 @@ let c;
 let canvasH;
 let scale;
 
+const BALL_COLOURS = ['#1D5C98', '#1D97CF', '#A8B336', '#0094A8', '#E2ECF0']
+// Starting Locations of the balls
+const startingBalls = [
+    {x: 0, y: 90, size: 100, colour: BALL_COLOURS[0], },
+    {x: 50, y: 400, size: 400, colour: BALL_COLOURS[1], },
+  ]
+ 
+function drawBall(ball) {
+  const { x, y, size, colour } = ball;
+  c.fillStyle = colour;
+  c.beginPath();
+  c.ellipse(x, y, size / 2, size / 2, 0, 0, 7);
+  c.fill();
+}
 
+function drawBalls() {
+ for (const ball of startingBalls) {
+    drawBall(ball);
+  }
+}
 
 function init() {
   const canvas = document.querySelector('canvas');
@@ -21,6 +40,8 @@ function init() {
   // Load and then draw the logo (white for each non-transparent pixel)
   prepareText(c);
   prepareLogo(c);
+
+  drawBalls();
 
 //  drawLandscape();
 //  saveCanvasPicture();
