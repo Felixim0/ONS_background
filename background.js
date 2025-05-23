7171ec5ca196acce2195d914e184cf31e0691292
Felixim0 <aldam.gatesfelix@gmail.com>
2025-05-24 00:49:38 +0100

    Adjusted starter ball values so that they're much more accurate to the real thing

--- background.js
+++ background.js
@@ -7,8 +7,14 @@ let scale;
 const BALL_COLOURS = ['#1D5C98', '#1D97CF', '#A8B336', '#0094A8', '#E2ECF0']
 // Starting Locations of the balls
 const startingBalls = [
-    {x: 0, y: 90, size: 100, colour: BALL_COLOURS[0], },
-    {x: 50, y: 400, size: 400, colour: BALL_COLOURS[1], },
+    {x: 0, y: 90, size: 100, colour: BALL_COLOURS[0], }, // Top Left Ball
+    {x: 50, y: 400, size: 450, colour: BALL_COLOURS[1], }, // Bottom Left Ball
+    // Top Right Balls 
+    // Top Right Balls End
+    {x: 730, y: 420, size: 320, colour: BALL_COLOURS[3], }, // bottom right ball
+    {x: 705, y: 255, size: 125, colour: BALL_COLOURS[2], }, // bottom right middle ball
+    {x: 650, y: 205, size: 88, colour: BALL_COLOURS[0], }, // bottom right ball
+
   ]
  
 function drawBall(ball) {
